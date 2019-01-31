2f828d18c016192e7c38571541be9016503240d6
DESKTOP-5PHUL53\adiwi <adi@datalabs.id>
2019-01-31 10:33:28 +0700

    Change Setting

--- settings/commons.py
+++ settings/commons.py
@@ -1,9 +1,9 @@
-PORT=9998
+PORT=2525
 
-host_hive = "209.97.167.105"
+host_hive = "178.128.114.101"
 host_http_hive= "http://"+host_hive
-username_hive = 'adi'
+username_hive = 'mapr'
 
-spark_host = "http://209.97.167.105:7777"
+spark_host = "http://209.97.172.254:7777"
 
 postgres_host = "209.97.167.105"
