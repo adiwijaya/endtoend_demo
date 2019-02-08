af020d097e804717fccfbb47535b055ffc08a119
DESKTOP-5PHUL53\adiwi <adi@datalabs.id>
2019-02-08 10:15:01 +0700

    change spark api port

--- settings/commons.py
+++ settings/commons.py
@@ -4,6 +4,6 @@ host_hive = "178.128.114.101"
 host_http_hive= "http://"+host_hive
 username_hive = 'mapr'
 
-spark_host = "http://209.97.172.254:7777"
+spark_host = "http://209.97.172.254:5555"
 postgres_host = "209.97.172.254"
 mysql_host = "209.97.172.254"
